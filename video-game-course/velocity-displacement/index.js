83cbdfe54bcf92f13e01e6e5d162f3919f64803a
Shalom0117 <shalomalexander707@gmail.com>
2023-04-19 00:57:22 +0000

    pushing velocity code

--- video-game-course/velocity-displacement/index.js
+++ video-game-course/velocity-displacement/index.js
@@ -1,5 +1,5 @@
 // TODO 4: Change *my-game-lib* to the name of your game lib
-(function(window, opspark, shalomRepository) {
+(function (window, opspark, shalomRepository) {
   const
     engine = opspark.V6().activateResize(),
     canvas = engine.getCanvas(),
@@ -9,7 +9,7 @@
     phyz = shalomRepository.phyz,
     numz = shalomRepository.numz;
 
-  
+
 
   const ship = assets.makeShip('#4286f4');
 
@@ -25,7 +25,7 @@
    * acting on it.  In our case, the ship's propulsion is 
    * acting against it on both axis, x and y.
    */
-     ship.update = function() {
+  ship.update = function () {
     /*
      * TODO 7: Use your game lib's phyz.updateVelocity() 
      * method to update the ship's velocity. The 
@@ -38,9 +38,9 @@
      * is available to you in this scope as, "this".
      * 2. What are the x and y forces acting on our ship?
      */
-     
-    shalomRepository.phyz.updateVelocity(this)
-    
+
+    shalomRepository.phyz.updateVelocity(this, this.propulsion, this.propulsion)
+
     // also check if the ship needs to rebound off a boundary //
     reboundCircularAssetInArea(this, canvas);
   };
@@ -56,7 +56,7 @@
     .activateTick();
 
   // listen for user pressing keys down //
-  document.onkeydown = function(event) {
+  document.onkeydown = function (event) {
     /*
      * Up arrow can be pressed in combo with other keys.
      * propulsion of 0.1 is set when ArrowUp is pressed.
@@ -78,11 +78,21 @@
   };
 
   // listen for user releasing keys //
-  document.onkeyup = function(event) {
+  document.onkeyup = function (event) {
     // TODO 13: How do we stop the application of forces?
-    
+
+    if (event.key === 'ArrowUp') {
+      ship.propulsion = 0;
+      ship.rotationalVelocity = 0;
+    }
+    if (event.key === 'ArrowLeft') {
+      ship.rotationalVelocity = 0;
+    } else if (event.key === 'ArrowRight') {
+      ship.rotationalVelocity = 0;
+    }
+
   };
-  
+
   function reboundCircularAssetInArea(body, area) {
     const
       radius = body.radius,
@@ -96,10 +106,11 @@
       // we've struck the right side of the area //
       body.x = right - radius;
       body.velocityX *= -1;
-    } else if ( /* TODO 9: Check if body's hit left side */ false ) {
+    } else if (body.x + radius < left) {
       // we've struck the left side of the area //
       // TODO 10: Code the reaction to hitting the left side
-      
+      body.x = left + radius;
+      body.velocityX *= -1;
     }
 
     // check for hit on top or bottom //
@@ -107,12 +118,13 @@
       // we've struck the right side of the area //
       body.y = top + radius;
       body.velocityY *= -1;
-    } else if ( /* TODO 11: Check if body's hit bottom */ false ) {
+    } else if (body.y - radius > bottom) {
       // we've struck the bottom of the area //
       // TODO 12: Code the reaction to hitting the bottom
-      
+      body.y = bottom + radius;
+      body.velocityY *= -1;
     }
   }
-  
+
   // TODO 3: replace *my-game-lib* with the name of your game lib //
 }(window, window.opspark, window.shalomRepository));
